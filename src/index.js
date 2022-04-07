8316b2ac951d00c492b76e5f1e98a1b201bbd2c8
marta-dunajko <103064095+marta-dunajko@users.noreply.github.com>
2022-04-07 18:49:36 +0200

    Created index.js file

---
+++ src/index.js
@@ -0,0 +1,42 @@
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#city");
+  let cityInput = document.querySelector("#city-input");
+  cityElement.innerHTML = cityInput.value;
+  let apiKey = "3f206a71acb676a29d8c59b6eebcbee9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let form = document.querySelector("#entercity");
+form.addEventListener("submit", search);
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("h2");
+  temperatureElement.innerHTML = `${temperature} °C`;
+}
+
+function giveDate() {
+  let now = new Date();
+  let h3 = document.querySelector("h3");
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  } else {
+    minutes = minutes + "";
+  }
+  h3.innerHTML = `${day} ${hours}:${minutes}`;
+}
+giveDate();
