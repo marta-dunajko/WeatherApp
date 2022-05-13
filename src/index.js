556d019112f5906b641686900ae56c24321eb716
marta-dunajko <103064095+marta-dunajko@users.noreply.github.com>
2022-05-13 18:38:30 +0200

    Removed all traces of the forecast once again + aligned the icon so it looks better now

--- src/index.js
+++ src/index.js
@@ -11,13 +11,6 @@ function search(event) {
 let form = document.querySelector("#entercity");
 form.addEventListener("submit", search);
 
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let apiKey = "3f206a71acb676a29d8c59b6eebcbee9";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureElement = document.querySelector("h2");
@@ -31,7 +24,6 @@ function showTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  getForecast(response.data.coord);
 }
 
 function giveDate() {
